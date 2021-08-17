2209e47a4b9ee5ffd3a9f7cc252dada4cf3a7a3f
shardev <aleksandarsarovic@yahoo.com>
2021-08-17 16:06:25 +0200

    19 - Changed controller to handle request by one method: startMovieReservation.

---
+++ modules/custom/movie/src/Controller/MovieReservationController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\movie\Controller;
+
+use Laminas\Diactoros\Response\JsonResponse;
+
+class MovieReservationController
+{
+
+  public function startMovieReservation()
+  {
+    $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('genre');
+    if (!isset($_GET['genre']) || $_GET['genre'] == 0) {
+      $node_ids = \Drupal::entityQuery('node')->condition('type', 'movie')->execute();
+    } else {
+      $node_ids = \Drupal::entityQuery('node')->condition('type', 'movie')->condition('field_movie_genre', $_GET['genre'])->execute();
+
+    }
+    $nodes = \Drupal\node\Entity\Node::loadMultiple($node_ids);
+
+    foreach ($terms as $term) {
+      $genres[] = array(
+        'id' => $term->tid,
+        'name' => $term->name
+      );
+    }
+
+    return array(
+      '#theme' => 'start_movie_reservation',
+      '#genres' => $genres,
+      '#movies' => $nodes
+    );
+  }
+
+  function getMoviesByGenre()
+  {
+
+    $jsonNodes = new JsonResponse($nodes);
+    $payload = $jsonNodes->getPayload();
+
+    return $payload;
+
+    foreach ($nodes as $node) {
+      $allowed_values = $node->getFieldDefinition('field_availabile_days')->getFieldStorageDefinition()->getSetting('allowed_values');
+      $day_values = $node->get('field_availabile_days')->getValue();
+
+      $days_available = [];
+      foreach ($day_values as $singleDay) {
+        $days_available[$singleDay["value"]] = $allowed_values[$singleDay["value"]];
+      }
+
+      $json[] = array(
+        'id' => $node->id(),
+        'title' => $node->label(),
+        'description' => $node->field_description->value,
+        'image' => file_create_url($node->field_movie_cover_image->entity->getFileUri()),
+        'days_available' => $days_available
+      );
+    }
+
+    return new JsonResponse($json);
+  }
+}
---
+++ sites/default/files/php/twig/611bbf64e96de_start-movie-reservation.h_T7LBUw5C-bNHVSgEUL4FU73GJ/baGlVuLGWjkqfRiBFXO_8XooPBcY0sPRSAadg1JD7GI.php
@@ -0,0 +1,291 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/custom/movie/templates/start-movie-reservation.html.twig */
+class __TwigTemplate_b55cfba5bc3651bffb63ebcff2ac73504fe9f6a8459c26be55a713cee0730d03 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<h4>Welcome to our movie reservation page</h4>
+
+<form action=\"#\">
+  <label for=\"movie-genre\">
+    Please select movie genre for which you would like to make a reservation:
+  </label>
+
+  <select name=\"movie-genre\" id=\"movie-genre\">
+    <option value=\"0\">All movies</option>
+    ";
+        // line 10
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["genres"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["genre"]) {
+            // line 11
+            echo "      <option value=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["genre"], "id", [], "any", false, false, true, 11), 11, $this->source), "html", null, true);
+            echo "\">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["genre"], "name", [], "any", false, false, true, 11), 11, $this->source), "html", null, true);
+            echo "</option>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['genre'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 13
+        echo "  </select>
+  <button id=\"searchButton\" type=\"button\">Search</button>
+</form>
+
+<div id=\"moviesByGenre\">
+</div>
+
+
+<style>
+  .active {
+    background-color: #285c91;
+  }
+
+  .inactive {
+    background-color: white;
+  }
+
+</style>
+<script>
+  //jQuery(document).ready(function)
+  jQuery(\"#searchButton\").click(function (){
+    var genre = document.getElementById('movie-genre').value;
+    console.log(genre);
+
+    jQuery.ajax({
+      url: \"/start-movie-reservation\",
+      type: \"get\",
+      data: {\"genre\": genre},
+      success: function (response) {
+        console.log(response)
+        //populateHtml(response);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+      }
+    });
+  })
+
+  function populateHtml(data) {
+    let html = \"\";
+
+    jQuery.each(data, function (k, v) {
+
+      if (v.days_available == null) {
+        v.days_available = 'Not available.'
+      }
+
+      let days_available_built = ''
+      for (let key in v.days_available){ days_available_built += v.days_available[key] + ' ' }
+
+      html += `<br><div class=\"inactive\" id=\"movie-\${v.id}\" onclick=\"toggleActive(this.id)\" > <h4>Title: \${v.title} </h4> <br> <img src=\"\${v.image}\"> <p>Description: \${v.description} </p> <p> Available days:` + days_available_built + `</p></div><hr>`
+    })
+
+    jQuery('#moviesByGenre').html(html);
+  }
+
+  function toggleActive(id){
+    var clickedDiv = document.getElementById(id)
+
+    if (clickedDiv.className == \"inactive\") {
+
+      var alreadyActive = document.getElementsByClassName(\"active\");
+      console.log(alreadyActive[0])
+
+      // Deactivate another active divs if exist
+      for (let i = 0; i < alreadyActive.length; i++) {
+        if (typeof alreadyActive[i] !== \"undefined\" && alreadyActive[i].id != clickedDiv.id) {
+          alreadyActive[i].className = \"inactive\"
+          jQuery('#reservationButton').remove()
+        }
+      }
+
+      clickedDiv.className = \"active\"
+      alreadyActive = []
+      jQuery('#'+id).after(`<div id=\"reservationButton\"><button>Reserve movie</button></div>`)
+
+    } else {
+      clickedDiv.className = \"inactive\"
+      jQuery('#reservationButton').remove()
+    }
+  }
+</script>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/movie/templates/start-movie-reservation.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  65 => 13,  54 => 11,  50 => 10,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<h4>Welcome to our movie reservation page</h4>
+
+<form action=\"#\">
+  <label for=\"movie-genre\">
+    Please select movie genre for which you would like to make a reservation:
+  </label>
+
+  <select name=\"movie-genre\" id=\"movie-genre\">
+    <option value=\"0\">All movies</option>
+    {% for genre in genres %}
+      <option value=\"{{ genre.id }}\">{{ genre.name }}</option>
+    {% endfor %}
+  </select>
+  <button id=\"searchButton\" type=\"button\">Search</button>
+</form>
+
+<div id=\"moviesByGenre\">
+</div>
+
+
+<style>
+  .active {
+    background-color: #285c91;
+  }
+
+  .inactive {
+    background-color: white;
+  }
+
+</style>
+<script>
+  //jQuery(document).ready(function)
+  jQuery(\"#searchButton\").click(function (){
+    var genre = document.getElementById('movie-genre').value;
+    console.log(genre);
+
+    jQuery.ajax({
+      url: \"/start-movie-reservation\",
+      type: \"get\",
+      data: {\"genre\": genre},
+      success: function (response) {
+        console.log(response)
+        //populateHtml(response);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+      }
+    });
+  })
+
+  function populateHtml(data) {
+    let html = \"\";
+
+    jQuery.each(data, function (k, v) {
+
+      if (v.days_available == null) {
+        v.days_available = 'Not available.'
+      }
+
+      let days_available_built = ''
+      for (let key in v.days_available){ days_available_built += v.days_available[key] + ' ' }
+
+      html += `<br><div class=\"inactive\" id=\"movie-\${v.id}\" onclick=\"toggleActive(this.id)\" > <h4>Title: \${v.title} </h4> <br> <img src=\"\${v.image}\"> <p>Description: \${v.description} </p> <p> Available days:` + days_available_built + `</p></div><hr>`
+    })
+
+    jQuery('#moviesByGenre').html(html);
+  }
+
+  function toggleActive(id){
+    var clickedDiv = document.getElementById(id)
+
+    if (clickedDiv.className == \"inactive\") {
+
+      var alreadyActive = document.getElementsByClassName(\"active\");
+      console.log(alreadyActive[0])
+
+      // Deactivate another active divs if exist
+      for (let i = 0; i < alreadyActive.length; i++) {
+        if (typeof alreadyActive[i] !== \"undefined\" && alreadyActive[i].id != clickedDiv.id) {
+          alreadyActive[i].className = \"inactive\"
+          jQuery('#reservationButton').remove()
+        }
+      }
+
+      clickedDiv.className = \"active\"
+      alreadyActive = []
+      jQuery('#'+id).after(`<div id=\"reservationButton\"><button>Reserve movie</button></div>`)
+
+    } else {
+      clickedDiv.className = \"inactive\"
+      jQuery('#reservationButton').remove()
+    }
+  }
+</script>
+", "modules/custom/movie/templates/start-movie-reservation.html.twig", "C:\\xampp\\htdocs\\drupal_first_test\\modules\\custom\\movie\\templates\\start-movie-reservation.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("for" => 10);
+        static $filters = array("escape" => 11);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
