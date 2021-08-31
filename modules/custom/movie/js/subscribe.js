c89173963ed4282942def127d7074a2431df8443
shardev <aleksandarsarovic@yahoo.com>
2021-08-31 11:25:18 +0200

    32 - Implemented client side validation.

--- modules/custom/movie/js/subscribe.js
+++ modules/custom/movie/js/subscribe.js
@@ -12,46 +12,59 @@ jQuery(function () {
 
   // Close first then open second popup dialog
   jQuery("#next1").click(function (event) {
-    firstName = jQuery("#firstName").val()
-    lastName = jQuery("#lastName").val()
-    emailInput = jQuery("#emailInput").val()
+    event.preventDefault()
+    if (jQuery("#form1")[0].checkValidity()) {
+      firstName = jQuery("#firstName").val()
+      lastName = jQuery("#lastName").val()
+      emailInput = jQuery("#emailInput").val()
 
-    jQuery(this).closest('#dialog-opt-1').dialog('close');
-    jQuery('#dialog-opt-2').dialog({
-      title: "Enter information:",
-      width: 500
-    })
+      jQuery(this).closest('#dialog-opt-1').dialog('close');
+      jQuery('#dialog-opt-2').dialog({
+        title: "Enter information:",
+        width: 500
+      })
+    }
   })
 
   // Close second then open third popup dialog
   jQuery("#next2").click(function (event) {
-    jQuery(this).closest('.ui-dialog-content').dialog('close');
-    jQuery('#dialog-opt-3').dialog({
-      title: "Enter information:",
-      width: 500
-    })
+    event.preventDefault()
+    if (jQuery("#form2")[0].checkValidity()) {
+      jQuery(this).closest('.ui-dialog-content').dialog('close');
+      jQuery('#dialog-opt-3').dialog({
+        title: "Enter information:",
+        width: 500
+      })
+    }
   })
 
   // Send request for save subscription
   jQuery("#next3").click(function (event) {
-    jQuery.ajax({
-      url: "/save-subscription",
-      type: "post",
-      data: {
-        "subscription_data": {
-          "first_name": firstName,
-          "last_name": lastName,
-          "email_address": emailInput
-        }
-      },
-      success: function (response) {
-        document.open()
-        document.write(response)
-        document.close()
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        jQuery("html").html("<p>Error occurred while making subscription. </br> <input type='submit' onclick=\"window.history.go(-1); return false;\">Go back</input> ")
+    event.preventDefault()
+    if (jQuery("#form3")[0].checkValidity()) {
+      if (jQuery("#email2").val() !== jQuery("#email3").val()) {
+        alert('Email Not Matching!');
+      } else {
+        jQuery.ajax({
+          url: "/save-subscription",
+          type: "post",
+          data: {
+            "subscription_data": {
+              "first_name": firstName,
+              "last_name": lastName,
+              "email_address": emailInput
+            }
+          },
+          success: function (response) {
+            document.open()
+            document.write(response)
+            document.close()
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            jQuery("html").html("<p>Error occurred while making subscription. </br> <input type='submit' onclick=\"window.history.go(-1); return false;\">Go back</input> ")
+          }
+        });
       }
-    });
+    }
   })
 })
